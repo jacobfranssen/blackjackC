ac3b19b77f29d7fdf9293e0f4e0e7518c54633e1
JADwars <97022737+JADwars@users.noreply.github.com>
2022-10-25 11:48:18 +0200

    Nieuwste versie

--- main_2.cpp
+++ main_2.cpp
@@ -106,151 +106,6 @@ public:
     }
 };
 
-class bot{
-public:
-    std:: string Name="name";
-    std::array<int, 21> hand;
-    int wallet =0;
-    int strategyA[22][22];
-    int strategydouble[22][22];
-    int strategyother[22][22];
-    std::string make_choice(int card_dealer){
-        int choice_num;
-        std::string choice;
-        if (hand[2]==0){ //if the bot has 2 cards go to cases underneath
-            if(hand[0]==hand[1]){ //if the bot has two similar cards:
-                choice_num=strategydouble[hand[0]] [card_dealer];//first value row, second value column
-            }else if(hand[0]=11){ //if bot has
-                choice_num=strategyA[hand[1]][card_dealer];
-            }else if(hand[1]=11){
-                choice_num=strategyA[hand[0]][card_dealer];
-            }else{
-                choice_num=strategyother[hand[0]+hand[1]][card_dealer];
-            }
-
-        }else{
-            choice_num=strategyother[this->calculate_score()][card_dealer];
-        }
-        switch (choice_num){
-            case 0:
-                choice="Hit";
-                std::cerr << "Determining strategy went wrong. bot goes for stand as default";
-                break;
-            case 1:
-                choice="Hit";
-                break;
-            case 2:
-                choice="Double";
-                break;
-            case 3:
-                choice="Stand";
-                break;
-            case 4:
-                choice="Split";
-                break;
-        }
-        
-        return choice;
-    }
-
-    void set_strategy(std::string strategy){
-        if(strategy=="Basic strategy") {
-            std::ifstream fileinA{"basic_strategy_containing_A.txt"};
-            std::ifstream fileinD{"basic_strategy_double_cards.txt"};
-            std::ifstream fileinO{"basic_strategy_other.txt"};
-            if (!fileinA.is_open()) {
-                std::cerr << "Error opening basic_strategy_containing_A.txt";
-                exit(-1);
-            };
-
-            if (!fileinD.is_open()) {
-                std::cerr << "Error opening basic_strategy_double_cards.txt";
-                exit(-1);
-            };
-            if (!fileinO.is_open()) {
-                std::cerr << "Error opening basic_strategy_other.txt";
-                exit(-1);
-            };
-            for(int j=0; j<23; j++) {
-                for (int i = 0; i < 23; i++) {
-                    fileinA >> strategyA[j][i];//first value row, second value column
-                    fileinD >> strategydouble[j][i];
-                    fileinO >> strategyother[j][i];
-                }
-            }
-
-        }else{
-            std::cerr << "Strategy not defined, choose Basic strategy";
-            exit(-1);
-        }
-    }
-    void set_name(std::string name){
-        Name=name;
-    };
-
-    void display_hand(){
-        std:: cout<< "The hand of " << Name << " is:\t" ;
-        for(int i=0;i<21;i++){
-            if (hand[i]!= 0){
-                std:: cout<<hand[i]<<", ";
-            }
-        }
-        std::cout<<std::endl;
-    };
-
-    void clear_hand(){
-        for(int i=0; i< hand.size(); i++){
-            hand[i]=0;
-        };
-    };
-
-    void get_card(int number) {
-        int i = 0;
-        while (hand[i] != 0) {
-            i = i + 1;
-        }
-        hand[i] = number;
-    }
-
-    int calculate_score(){
-        //int score=0;
-        int index= 0;
-        for(int value:hand){
-            if(value==11){
-                int score= 0;
-                for(int value2:hand){
-                    score=score+value2;
-
-                }
-                if(score>21){
-                    hand[index]=1;
-                }
-                index=index+1;
-            }
-
-
-        }
-        int score=0;
-        for(int value:hand){
-            score=score+value;
-        }
-        return score;
-    }
-
-    int place_bet(int bet){
-        if(bet>wallet){
-            std::cout<< Name<<"has not enough money, wallet runs negative"<<std::endl;
-        }
-        wallet=wallet-bet;
-        return bet;
-    };
-
-    void increase_wallet(int amount){
-        wallet=wallet+amount;
-    }
-
-};
-
 class player{
 public:
 
@@ -311,8 +166,6 @@ public:
             score=score+value;
         }
 
-        std::cout << "The added values of your cards is: " << score << std::endl;
-
         if(score > 21){
             score_above_21 = 1;
         }
@@ -336,27 +189,11 @@ public:
 
 class Blackjack{
 public:
-    std::vector<player> players;
     int number_of_players=0;
-    int players_with_name=0;
-
-    void import_player(player player_n){
-        players[number_of_players]=player_n;
-        number_of_players+=1;
-    };
-    void give_names(){
-        std::string name;
-        for(int i=0;i<players.size();i++)
-            std::cout<<"Enter the name of player"<<players_with_name+1<<std::endl;
-        std::cin>>name;
-        players[players_with_name].set_name(name);
-        players_with_name+=1;
-    }
 
     void set_number_of_players(int number){
         number_of_players = number;
     }
-
 };
 
 int initialisation_amount_of_players(){
@@ -442,13 +279,20 @@ int main() {
 
         for(int n = 0; n < game.number_of_players; n++){
             std::string choice = "Y"; 
-            std::cout << players[n].Name << ", do you want to draw another card? " << std::endl;
-            std::cin >> choice;
+            if(players[n].calculate_score() != 21){
+                std::cout << players[n].Name << ", do you want to draw another card? " << std::endl;
+                std::cin >> choice;
+            }
+            else{
+                choice = "N";
+            }
 
             while(choice == "Y"){
                 players[n].get_card(deck.draw_card());
                 players[n].calculate_score();
-                if(players[n].score_above_21 == 0){
+                int score = players[n].calculate_score();
+                if(players[n].score_above_21 == 0 && score != 21){
+                    std::cout << "The added value of your hand is now: " << score << std::endl;
                     std::cout << players[n].Name << ", do you want to draw another card? " << std::endl;
                     std::cin >> choice;
                 }
@@ -460,19 +304,31 @@ int main() {
             }
         }
 
+        while(Dealer.calculate_score() < 16){
+            Dealer.get_card(deck.draw_card());
+        }
+
+        if(Dealer.calculate_score() > 21){
+            Dealer.clear_hand();
+            std::cout << "The dealer is busted, he exceeded 21." << std::endl;
+        }
+
         for(int o = 0; o < game.number_of_players; o++){
             int score = players[o].calculate_score();
             int score_dealer = Dealer.calculate_score();
-            if(players[o].score_above_21 == 0 && score > score_dealer){
+            if(players[o].score_above_21 == 0 && score >= score_dealer){
                 std::cout << players[o].Name << " congratulations you've beaten the dealer!" << std::endl;
+                std::cout << std::endl;
             }
             else{
-                std::cout << players[o].Name << " You messed it up!" << std::endl;
+                std::cout << players[o].Name << " You unfortunately lost, your score was: " << score << std::endl;
+                std::cout << std::endl;
             }
         }
 
         int score_dealer = Dealer.calculate_score();
         std::cout << "this is the score of the dealer: " << score_dealer << std::endl;
+        std::cout << std::endl;
 
         std::cout << "do you want to continue the game, if yes type 1, if no type 0. " << std::endl;
         std::cin >> continue_game;
