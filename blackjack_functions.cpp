649b69f9cb41229553e30045d54f309b78e9fc58
JADwars <97022737+JADwars@users.noreply.github.com>
2022-10-31 10:48:38 +0100

    Presentatie versie

--- blackjack_functions.cpp
+++ blackjack_functions.cpp
@@ -5,16 +5,19 @@
 #include "class_carddeck.h"
 #include "class_dealer.h"
 #include "class_player.h"
+#include <typeinfo>
+#include <stddef.h>
+#include <limits.h>
 
 void Blackjack::initialisation_amount_of_players()
 {
-    int Number = 0;
-    while (Number <= 0 || Number >= 10)
-    {
-        std::cout << "How many players will be attending this game? Please insert a number between 1 and 10. Insert the amount here: " << std::endl;
-        std::cin >> Number;
-        std::cout << std::endl;
-    }
+    int Number;
+    bool valid = false;
+
+    std::cout << "How many players will be attending this game? Please insert a number between 1 and 10. Insert the amount here: " << std::endl;
+    std::cin >> Number;
+    std::cout << std::endl;
+
     Amount_of_Players = Number;
 }
 
@@ -44,7 +47,8 @@ void Blackjack::initialisation_number_of_decks()
     }
 }
 
-std::string Blackjack::raise_question_user_extra_card(std::string Pos_answer_1, std::string Pos_answer_2, std::string player_name){
+std::string Blackjack::raise_question_user_extra_card(std::string Pos_answer_1, std::string Pos_answer_2, std::string player_name)
+{
     std::string choice_player = "--";
     std::string choice = "-";
 
@@ -74,7 +78,8 @@ void Blackjack::play()
 
     while (continue_game == 1)
     {
-
+        int Amount_of_busted_players = 0;
+        bool Allplayersbusted = 0;
         Dealer.clear_hand(); // clearing the hand of the dealer at the start
 
         for (int j = 0; j < Amount_of_Players; j++)
@@ -96,14 +101,16 @@ void Blackjack::play()
             Dealer.display_second_card();
         }
 
-        for (int l = 0; l < 2; l++){
+        for (int l = 0; l < 2; l++)
+        {
             for (int k = 0; k < Amount_of_Players; k++)
             {
                 players[k].get_card(deck.draw_card());
             }
         }
 
-        for (int m = 0; m < Amount_of_Players; m++){
+        for (int m = 0; m < Amount_of_Players; m++)
+        {
             players[m].display_hand();
 
             if (players[m].calculate_score() == 21)
@@ -116,7 +123,8 @@ void Blackjack::play()
         {
             std::string choice = "--";
             std::string choice_player = "--";
-            if (players[n].calculate_score() != 21){
+            if (players[n].calculate_score() != 21)
+            {
                 choice = raise_question_user_extra_card("Yes", "No", players[n].Name);
             }
 
@@ -125,51 +133,73 @@ void Blackjack::play()
                 players[n].get_card(deck.draw_card());
                 players[n].calculate_score();
                 int score = players[n].calculate_score();
-                if (players[n].score_above_21 == 0){
+                if (players[n].score_above_21 == 0)
+                {
                     std::cout << "The added value of your hand is now: " << score << std::endl;
                     choice = raise_question_user_extra_card("Yes", "No", players[n].Name);
                 }
-                else{
+                else
+                {
                     std::cout << "You unfortunately passed 21 and are therefor busted, the added value of your hand is " << score << std::endl;
                     std::cout << std::endl;
+                    Amount_of_busted_players++;
+                    if (Amount_of_busted_players == Amount_of_Players)
+                    {
+                        std::cout << "Unfortunately every player passed 21, this round ends here. " << std::endl;
+                        Allplayersbusted = 1;
+                    }
                     choice = "N";
                 }
             }
         }
 
-        while (Dealer.calculate_score() < 16)
+        if (Allplayersbusted != 1)
         {
-            Dealer.get_card(deck.draw_card());
-        }
-
-        if (Dealer.calculate_score() > 21)
-        {
-            Dealer.clear_hand();
-            std::cout << std::endl;
-            std::cout << "The dealer is busted, he exceeded 21." << std::endl;
-        }
-
-        for (int o = 0; o < Amount_of_Players; o++)
-        {
-            int score = players[o].calculate_score();
-            int score_dealer = Dealer.calculate_score();
-            if (players[o].score_above_21 == 0 && score >= score_dealer)
+            while (Dealer.calculate_score() < 16)
             {
-                std::cout << players[o].Name << " congratulations you've beaten the dealer!" << std::endl;
+                Dealer.get_card(deck.draw_card());
+            }
+
+            if (Dealer.calculate_score() > 21)
+            {
+                Dealer.clear_hand();
                 std::cout << std::endl;
+                std::cout << "The dealer is busted, he exceeded 21." << std::endl;
+            }
+
+            for (int o = 0; o < Amount_of_Players; o++)
+            {
+                int score = players[o].calculate_score();
+                int score_dealer = Dealer.calculate_score();
+                if (players[o].score_above_21 == 0 && score >= score_dealer)
+                {
+                    std::cout << players[o].Name << " congratulations you've beaten the dealer!" << std::endl;
+                    std::cout << std::endl;
+                }
+                else
+                {
+                    std::cout << players[o].Name << " You unfortunately lost, your score was: " << score << std::endl;
+                    std::cout << std::endl;
+                }
+            }
+
+            std::cout << "The score of the dealer was: " << Dealer.calculate_score() << std::endl;
+        }
+
+        std::string choice_player = "--";
+        while (!(choice_player == "Yes" or choice_player == "No"))
+        {
+            std::cout << "do you want to continue the game, Enter Yes or No " << std::endl;
+            std::cin >> choice_player;
+            if (choice_player == "Yes")
+            {
+                continue_game = true;
             }
             else
             {
-                std::cout << players[o].Name << " You unfortunately lost, your score was: " << score << std::endl;
-                std::cout << std::endl;
+                continue_game = false;
             }
         }
-
-        int score_dealer = Dealer.calculate_score();
-        std::cout << "this is the score of the dealer: " << score_dealer << std::endl;
-        std::cout << std::endl;
-
-        std::cout << "Do you want to continue the game, if yes type 1, if no type 0. " << std::endl;
-        std::cin >> continue_game;
+        choice_player = "--";
     }
 }
