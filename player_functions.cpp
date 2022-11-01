4932c018a2c2e8c795dc69570f95787fd103adba
jacobfranssen <jacobfranssen@hotmail.nl>
2022-11-01 16:04:02 +0100

    jacob

--- player_functions.cpp
+++ player_functions.cpp
@@ -21,6 +21,7 @@ void player::display_hand(){
 void player::clear_hand(void){
     for (int i = 0; i < hand.size(); i++){
         hand[i] = 0;
+        score_above_21 = 0;
     };
 }
 
@@ -32,31 +33,28 @@ void player::get_card(int number){
     hand[i] = number; // place the card at the first zero location
 }
 
-int player::calculate_score(){
+int player::calculate_score() {
     int index = 0; // this number indicates the place in the hand that is being evaluated
-    for (int value : hand){ // loop through the hand
-        if (value == 11){ // if an ace is located in the hand it could either be an 11 or a 1
+    for (int value: hand) { // loop through the hand
+        if (value == 11) { // if an ace is located in the hand it could either be an 11 or a 1
             int score = 0; // score of the hand if an ace is counted as 11 is calculated here
-            for (int value2 : hand){ // calculate the score of the hand if ace would be 11
+            for (int value2: hand) { // calculate the score of the hand if ace would be 11
                 score = score + value2;
             }
-            if (score > 21){ // if the score of the hand turns out to be higher than 21, the ace turns into a 1
+            if (score > 21) { // if the score of the hand turns out to be higher than 21, the ace turns into a 1
                 hand[index] = 1;
             }
             index = index + 1; // move to the next index of the hand
         }
     }
     int score = 0;
-    for (int value : hand){ // calculate the score of the hand again with the aces being the righ value
+    for (int value: hand) { // calculate the score of the hand again with the aces being the righ value
         score = score + value;
     }
-
-    if (score > 21){
+    if (score > 21) {
         score_above_21 = 1;
     }
-
-    return score;
-}
+    return score;}
 
 int player::place_bet(int bet){
     if (bet > wallet){
